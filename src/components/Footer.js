f9860fab619b5454ddb5565acd95d5b667d0d36f
aashraf09 <asad15112007@gmail.com>
2024-03-20 02:34:38 +0500

    fixed opacity of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
     }, [])
     return (
         <footer className='min-h-[50vh] relative mt-[40px] w-full'>
-            <section className="md:w-[80%] w-[90%] footer md:mt-[80px] flex items-center justify-center min-h-[50vh] z-40">
+            <section className="md:w-[80%] w-[90%] footer md:mt-[80px] flex items-center justify-center min-h-[50vh] z-50">
                 <div className="w-[80%] m-auto flex flex-col md:flex-row items-center justify-start gap-10 md:gap-20">
                     <h3>{brandName.brandTitle}</h3>
                     <div className="flex flex-col items-start justify-between gap-2 text-center">
